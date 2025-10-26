e50298dc6b3da284edf21b79abc9ebb76a7b0cf9
Gagan jain004 <gagan.praveen004@gmail.com>
2025-10-26 21:11:04 +0530

    #Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes');
+const cors = require('cors');
 
 const app = express()                   // it creates server (not start server)
 
+
+app.use(cors());                      // to handle cors error
 app.use(express.json());              // to parse json body
 
 app.get('/',(req,res)=>{
